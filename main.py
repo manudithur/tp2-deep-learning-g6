482ec130a841c2dacee924605e733eb99d96d5dd
Manuel Dithurbide <manudithurbide@teclaacademy.com>
2024-10-01 17:48:57 -0300

    transformed metrics into a class

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from dotenv import load_dotenv, find_dotenv
 from src.models import Profile, DIET_TYPE, GENDER, ACTIVITY_LEVEL, FITNESS_GOAL, RESTRICTIONS
 from src.llm import LLM
-from src.metrics import evaluate_contextual_recall, evaluate_faithfulness, evaluate_answer_relevancy, evaluate_contextual_precision
+from src.metrics import Metrics
 _ = load_dotenv(find_dotenv())
 
 llm = LLM()
@@ -33,7 +33,8 @@ print(response+"\n")
 
 expected_response = "A banana contains 300 calories, 0.4 grams of fat, 27 carbohydrates, and 1.2 grams of protein."
 
-evaluate_contextual_precision(prompt, response, retrieved_docs, expected_response)
+metrics = Metrics(prompt, response, retrieved_docs, expected_response)
 
-evaluate_contextual_recall(prompt, response, retrieved_docs, expected_response)
+metrics.evaluate_contextual_precision()
+metrics.evaluate_contextual_recall()
 
--- src/metrics.py
+++ src/metrics.py
@@ -4,74 +4,61 @@ from deepeval.test_case import LLMTestCase
 from deepeval.metrics import AnswerRelevancyMetric, FaithfulnessMetric, HallucinationMetric, ContextualRelevancyMetric, \
     ContextualPrecisionMetric
 
+class Metrics:
 
-def evaluate_faithfulness(prompt, response, retrieved_docs):
-    test_case = LLMTestCase(
-        input=prompt,
-        actual_output=response,
-        retrieval_context=retrieved_docs
-    )
-    #FAITHFULNESS METRIC
-    metric = FaithfulnessMetric(
-        threshold=0.7,
-        model="gpt-4o",
-        include_reason=True
-    )
-    metric.measure(test_case)
-    print("Faithfulness Metric:\n")
-    print(metric.score)
-    print(metric.reason)
+    def __init__(self, prompt, actual_output, retrieved_docs, expected_output):
+        self.test_case = LLMTestCase(
+            input=prompt,
+            actual_output=actual_output,
+            expected_output=expected_output,
+            retrieval_context=retrieved_docs
+        )
+        
+    def evaluate_faithfulness(self):
+        #FAITHFULNESS METRIC
+        metric = FaithfulnessMetric(
+            threshold=0.7,
+            model="gpt-4o",
+            include_reason=True
+        )
+        metric.measure(self.test_case)
+        print("Faithfulness Metric:\n")
+        print(metric.score)
+        print(metric.reason)
 
-def evaluate_answer_relevancy(prompt, response, retrieved_docs):
-    test_case = LLMTestCase(
-        input=prompt,
-        actual_output=response,
-        retrieval_context=retrieved_docs
-    )
-    #ANSWER RELEVANCY
-    metric = AnswerRelevancyMetric(
-        threshold=0.7,
-        model="gpt-4o",
-        include_reason=True
-    )
-    metric.measure(test_case)
-    print("Answer Relevancy Metric:\n")
-    print(metric.score)
-    print(metric.reason)
+    def evaluate_answer_relevancy(self):
+        #ANSWER RELEVANCY
+        metric = AnswerRelevancyMetric(
+            threshold=0.7,
+            model="gpt-4o",
+            include_reason=True
+        )
+        metric.measure(self.test_case)
+        print("Answer Relevancy Metric:\n")
+        print(metric.score)
+        print(metric.reason)
 
-def evaluate_contextual_precision(prompt, response, retrieved_docs, expected_output):
-    test_case = LLMTestCase(
-        input=prompt,
-        actual_output=response,
-        expected_output=expected_output,
-        retrieval_context=retrieved_docs
-    )
-    #CONTEXTUAL PRECISION
-    metric = ContextualPrecisionMetric(
-        threshold=0.7,
-        model="gpt-4o",
-        include_reason=True
-    )
-    metric.measure(test_case)
-    print("Contextual Precision Metric:\n")
-    print(metric.score)
-    print(metric.reason)
+    def evaluate_contextual_precision(self):
+        #CONTEXTUAL PRECISION
+        metric = ContextualPrecisionMetric(
+            threshold=0.7,
+            model="gpt-4o",
+            include_reason=True
+        )
+        metric.measure(self.test_case)
+        print("Contextual Precision Metric:\n")
+        print(metric.score)
+        print(metric.reason)
 
-def evaluate_contextual_recall(prompt, response, retrieved_docs, expected_output):
-    test_case = LLMTestCase(
-        input=prompt,
-        actual_output=response,
-        expected_output=expected_output,
-        retrieval_context=retrieved_docs
-    )
-    #CONTEXTUAL RELEVANCY
-    metric = ContextualRelevancyMetric(
-        threshold=0.7,
-        model="gpt-4o",
-        include_reason=True
-    )
-    metric.measure(test_case)
-    print("Contextual Relevancy Metric:\n")
-    print(metric.score)
-    print(metric.reason)
+    def evaluate_contextual_recall(self):
+        #CONTEXTUAL RELEVANCY
+        metric = ContextualRelevancyMetric(
+            threshold=0.7,
+            model="gpt-4o",
+            include_reason=True
+        )
+        metric.measure(self.test_case)
+        print("Contextual Relevancy Metric:\n")
+        print(metric.score)
+        print(metric.reason)
 
